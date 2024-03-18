e172984bc14c6e4f1bd0962542db08488ab02917
Phil Pablo Pfeil <phil.pfeil@kit.edu>
2024-03-18 22:48:35 +0100

    Resolve warnings; extract strings

--- app/src/main/java/info/z10/z10mobile/AutoPublisher.kt
+++ app/src/main/java/info/z10/z10mobile/AutoPublisher.kt
@@ -1,15 +1,10 @@
 package info.z10.z10mobile
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 
 class AutoPublisher : Fragment() {
     override fun onCreateView(
